a43327f60dcad69b622429b5607ddb68e23ca497
HI MESH <heem.ace@gmail.com>
2022-08-26 17:31:05 +0545

    changes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,80 @@
 import React from "react";
 import "./About.css";
+import about from "../../Assets/about-img.jpg";
+import { BsCheckLg } from "react-icons/bs";
 
 export default function About() {
   return (
     <React.Fragment>
-      <div>HAHA</div>
+      <div className="about" id="about">
+        <div className="image">
+          <img src={about} alt="" />
+        </div>
+
+        <div className="content">
+          <span>about us</span>
+          <h3 className="title">Every day is a chance to become better</h3>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione
+            quia accusamus dicta inventore nobis obcaecati vero odio, id
+            dolorum. Consequatur ex, aperiam deserunt nostrum perferendis illum
+            unde ipsa? Consequatur, distinctio?
+          </p>
+          <div className="box-container">
+            <div className="box">
+              <h3>
+                <i className="">
+                  <BsCheckLg />
+                </i>
+                body and mind
+              </h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Est,
+                enim.
+              </p>
+            </div>
+            <div className="box">
+              <h3>
+                <i className="">
+                  <BsCheckLg />
+                </i>
+                healthy life
+              </h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Est,
+                enim.
+              </p>
+            </div>
+            <div className="box">
+              <h3>
+                <i className="">
+                  <BsCheckLg />
+                </i>
+                strategies
+              </h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Est,
+                enim.
+              </p>
+            </div>
+            <div className="box">
+              <h3>
+                <i className="">
+                  <BsCheckLg />
+                </i>
+                workout
+              </h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Est,
+                enim.
+              </p>
+            </div>
+          </div>
+          <a href="#" className="btn">
+            read more
+          </a>
+        </div>
+      </div>
     </React.Fragment>
   );
 }
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,26 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavBar.css";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 export default function NavBar() {
+  const [nav, setNav] = useState(false);
+
   return (
     <React.Fragment>
-      <div className="header">
+      <nav className="navbar">
+        <h3 className="logo">
+          <span>HB</span>FITNESS{" "}
+        </h3>
+        <ul
+          className={nav ? "nav-links-mobile" : "nav-links"}
+          onClick={() => setNav(false)}
+        >
+          <li className="nav-items">
+            <a href="#home">Home </a>
+          </li>
+          <li className="nav-items">
+            <a href="#about">About Us </a>
+          </li>
+          <li className="nav-items">
+            <a href="#home">Features </a>
+          </li>
+          <li className="nav-items">
+            <a href="#home">Pricing </a>
+          </li>
+          <li className="nav-items">
+            <a href="#home">trainers </a>
+          </li>
+          <li className="nav-items">
+            <a href="#home">Blogs </a>
+          </li>
+        </ul>
+        <button className="mobile-menu-icon" onClick={() => setNav(!nav)}>
+          {nav ? <AiOutlineClose /> : <AiOutlineMenu />}
+        </button>
+      </nav>
+      {/* <div className="header">
         <a href="#" className="logo">
           {" "}
           <span>HB</span>FITNESS{" "}
         </a>
 
-        <div id="menu-btn" className="fas fa-bars"></div>
-
         <nav className="navbar">
-          <a href="#home">home</a>
-          <a href="#about">about</a>
-          <a href="#features">features</a>
-          <a href="#pricing">pricing</a>
-          <a href="#trainers">trainers</a>
-          <a href="#blogs">blogs</a>
+          <ul className={nav ? "nav-links-mobile" : "nav-links"}>
+            <a href="#home" className="nav-items">
+              home
+            </a>
+            <a href="#about" className="nav-items">
+              about
+            </a>
+            <a href="#features" className="nav-items">
+              features
+            </a>
+            <a href="#pricing" className="nav-items">
+              pricing
+            </a>
+            <a href="#trainers" className="nav-items">
+              trainers
+            </a>
+            <a href="#blogs" className="nav-items">
+              blogs
+            </a>
+          </ul>
         </nav>
-      </div>
+
+        <div className="mobile-menu-icon">
+          {nav ? (
+            <AiOutlineMenu/>
+          ) : (
+            <AiOutlineClose />
+          )}
+        </div>
+      </div> */}
     </React.Fragment>
   );
 }
